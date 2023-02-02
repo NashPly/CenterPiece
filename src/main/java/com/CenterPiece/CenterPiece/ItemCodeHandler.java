d6818d6aaae0dc1748d68f6562c0075570ba48cb
NashPly <tbeals@nashvilleplywood.com>
2023-02-02 07:23:43 -0600

    Adjusted search frame

--- src/main/java/com/CenterPiece/CenterPiece/ItemCodeHandler.java
+++ src/main/java/com/CenterPiece/CenterPiece/ItemCodeHandler.java
@@ -93,8 +93,8 @@ public class ItemCodeHandler {
         innerRequestBody.put("IncludeInvoicedOrders", false);
         innerRequestBody.put("IncludeCanceledOrders", false);
         innerRequestBody.put("OrderDateRangeStart", timeHandler.getCurrentYear() + "-" + timeHandler.getCurrentMonth() + "-" +
-                //timeHandler.getCurrentDayOfMonth() + "T00:00:01-6:00");
-                "01T00:00:01-6:00");
+                timeHandler.getCurrentDayOfMonth() + "T00:00:01-6:00");
+                //"01T00:00:01-6:00");
         innerRequestBody.put("OrderDateRangeEnd", timeHandler.getCurrentYear() + "-" + timeHandler.getCurrentMonth() + "-" +
                 timeHandler.getCurrentDayOfMonth() + "T"+"23:59:59-6:00");
 
